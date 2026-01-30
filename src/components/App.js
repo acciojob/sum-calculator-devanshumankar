87027d96df57f58336a3ea307c2eb9834b5f654f
devanshumankar <devanshumankar@gmail.com>
2026-01-30 21:44:21 +0530

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+	const [num, setNum] = useState(0);
+	const [sum, setSum] = useState(0);
 
-export default App
+	useEffect(() => {
+		setSum((prev) => prev + Number(num));
+	}, [num]);
+	return (
+		<div>
+			<h1>Sum Calculator</h1>
+			<input
+				type="number"
+				onChange={(e) => setNum(e.target.value)}
+				value={num}
+			></input>
+			<p>Sum: {sum}</p>
+			{/* Do not remove the main div */}
+		</div>
+	);
+};
+
+export default App;
